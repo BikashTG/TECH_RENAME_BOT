41b820c1cea82865d443476de978fd332f05b5b5
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-11-18 20:01:50 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -10,7 +10,7 @@ async def force_sub(bot, message):
     try:
        invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
     except ChatAdminRequired:
-       bot.log.exception("Make sure Bot is admin in Forcesub channel")
+       bot.log.exception("Make Sure Bot Is Admin In Forcesub Channel")
        Config.AUTH_CHANNEL = None 
        return
     button = [[
@@ -18,17 +18,17 @@ async def force_sub(bot, message):
                     "🤖 Join Updates Channel", url=invite_link.invite_link)
        ]]
     await message.reply_text(
-       text="<b>Please Join My Updates Channel to use me!</b>",
+       text="<b>Please Join My Updates Channel To Use Me !!</b>",
        reply_markup=InlineKeyboardMarkup(button))
     
 @Client.on_message(filters.private & filter.is_banned_user)
 async def ban_message(bot, message):
-    await message.reply_text(f"Sorry dude you were banned from using me.")
+    await message.reply_text(f"Sorry Dude 😔, You Were Banned From Using Me 🚫\nContact My Support Group")
     
 @Client.on_message(filters.command('ban') & filters.user(Config.OWNER_ID))
 async def ban(bot, message):
     if len(message.command) < 2:
-       return await message.reply("Give me a user id")
+       return await message.reply("Give Me A User Id")
     split = message.text.split()
     reason = "No reason provided"
     user_id = split[1]
@@ -41,10 +41,10 @@ async def ban(bot, message):
     except Exception as e:
        return await message.reply(f"Error: `{e}`")
     if user.id in Config.BANNED_USERS:
-       return await message.reply_text(f"{user.mention} is already banned")
+       return await message.reply_text(f"{user.mention} Is Already Banned")
     await db.ban_user(user.id, reason)
     Config.BANNED_USERS.append(user.id)
-    await message.reply_text(f"{user.mention} is Successfully banned")
+    await message.reply_text(f"{user.mention} Is Successfully Banned")
     
 @Client.on_message(filters.command('unban') & filters.user(Config.OWNER_ID))
 async def unban(bot, message):
@@ -58,7 +58,7 @@ async def unban(bot, message):
     except Exception as e:
       return await message.reply(f"Error: `{e}`")
     if user.id not in Config.BANNED_USERS:
-       return await message.reply(f"{user.mention} is not banned yet")
+       return await message.reply(f"{user.mention} Is Not Banned Yet")
     await db.unban_user(user.id)
     Config.BANNED_USERS.remove(user.id)
-    await message.reply_text(f"{user.mention} is Successfully unbanned")
+    await message.reply_text(f"{user.mention} Is Successfully Unbanned")
