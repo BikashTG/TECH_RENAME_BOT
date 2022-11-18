45b748dbd84af7ab836725ccdf6820b590b4d848
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-11-18 19:11:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://github.com/Mdbotz/md-renamebot /md-renamebot
-cd /md-renamebot 
+git clone https://github.com/BikashTG/TECH_RENAME_BOT /TECH_RENAME_BOT
+cd /TECH_RENAME_BOT 
 echo "installing requirements"
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
